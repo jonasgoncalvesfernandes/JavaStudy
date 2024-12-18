bb85ac26fa46fb5beb63dcfa3711bae441a7c195
Jonas Gonçalves <166181509+jonasgoncalvesfernandes@users.noreply.github.com>
2024-12-18 00:42:48 -0300

    Update FilmeView.java
    Corrigindo Funções de exibição

--- Utilizando_Herança/view/FilmeView.java
+++ Utilizando_Herança/view/FilmeView.java
@@ -4,16 +4,23 @@ import model.Conteudo;
 import model.Filme;
 import model.Serie;
 
+import java.util.*;
+
 public class FilmeView {
     public static void main(String[] args) {
         Filme p1 = new Filme("Norbet", 1.30, "Rasputia", 100000);
         Filme p2 = new Filme("Cidade de Deus", 1.59, "AK-47", 20000);
 
-        Serie s1 = new Serie("Atlanta", 1.30, 30, 40);
-        Serie s2 = new Serie("Atlanta", 1.30, 30, 40);
+        Serie s1 = new Serie("Breaking Bad", 90, 30, 40);
+        Serie s2 = new Serie("Atlanta", 80, 30, 40);
 
         p1.exibirInfo();
+        System.err.println();
         p2.exibirInfo();
+        System.err.println();
+        s1.exibirInfo();
+        System.err.println();
+        s2.exibirInfo();
 
     }
     
